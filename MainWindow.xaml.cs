0112148e0ca20b4c5fe01ee74e8847e1b5ddade5
Alexey <boronin_1993@mail.ru>
2023-12-10 14:11:37 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10._12._2023
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            //RadioButton rd = new RadioButton { IsChecked = true, GroupName="Languages",Content="JavaScript"};
+            //rd.Checked += RadioButton_Checked;
+            //stackPanel.Children.Add(rd);
+        }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Действие выполнено");
+        }
+
+        private void escButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton pressed=(RadioButton)sender;
+            MessageBox.Show(pressed.ToString());
+        }
+    }
+}
